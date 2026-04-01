632c1476067783c11fe746198e9c497647a15761
Yuriy <pe4enka055@gmail.com>
2026-04-01 18:58:07 +0300

    Доработки верстки

--- src/features/Header/index.tsx
+++ src/features/Header/index.tsx
@@ -10,14 +10,13 @@ export default function Header() {
   const toggleMenu = useMenuStore((s) => s.toggleMenu);
 
   return (
-    <header
-      className="fixed top-0 left-0 w-full h-20 max-sm:h-16 
-bg-deepBlue/90 backdrop-blur-md
-pt-[env(safe-area-inset-top)]
-z-50"
-    >
+    <header className="fixed top-0 left-0 w-full h-20 max-sm:h-16 z-50">
+      {/* фон header */}
+      <div className="absolute inset-0 bg-deepBlue/90 backdrop-blur-md"></div>
+
+      {/* safe-area сверху */}
+      <div className="h-[env(safe-area-inset-top)] bg-deepBlue/90 backdrop-blur-md"></div>
       <div className="container h-full flex items-center justify-between relative">
-        {/* <div className="fixed top-0 left-0 w-screen h-20 max-sm:h-16 bg-deepBlue z-[1000] pt-[env(safe-area-inset-top)]" /> */}
         <Link to="/" className="cursor-pointer" onClick={toggleMenu}>
           <p className="text-4xl max-sm:text-xl font-nk! text-white uppercase">
             Тех.Путь
