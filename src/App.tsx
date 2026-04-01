53e3a841b62ccd28f6a097c7dde33c1645b786e4
Yuriy <pe4enka055@gmail.com>
2026-04-01 19:05:43 +0300

    Доработки верстки

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ export default function App() {
     (state) => state.isRegisterModalOpen,
   );
   return (
-    <div className="flex-1 overflow-y-auto pt-20 max-sm:pt-16 [webkit-overflow-scrolling:touch]">
+    <div className="flex-1 overflow-y-auto [webkit-overflow-scrolling:touch]">
       <Header />
       <RoutesComponent />
       <Footer />
