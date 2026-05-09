24996dc7943b3f0f39352306580f5295ada0dd77
Yuriy <pe4enka055@gmail.com>
2026-05-09 20:07:01 +0300

    v

--- src/pages/AIAssistant/api/analyzeWeld.ts
+++ src/pages/AIAssistant/api/analyzeWeld.ts
@@ -22,7 +22,7 @@ export const analyzeWeld = async (file: File): Promise<AnalyzeWeldResponse> => {
   formData.append("image", file);
 
   const { data } = await axios.post<AnalyzeWeldResponse>(
-    "https://techput-production.up.railway.app/api/analyze-weld",
+    "techput-production-7a9e.up.railway.app/api/analyze-weld",
     formData,
   );
 
